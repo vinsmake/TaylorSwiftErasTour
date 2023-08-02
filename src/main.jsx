036b395839ed5d91c647f8fd14b48bbce3d4867a
vinsmake <ps.enrique.e@gmail.com>
2023-08-01 22:11:44 -0600

    Project updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { TaylorSwiftErasTour } from './TaylorSwiftErasTour'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TaylorSwiftErasTour />
   </React.StrictMode>,
 )
