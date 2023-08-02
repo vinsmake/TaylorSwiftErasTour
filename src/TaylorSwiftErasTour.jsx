642f6757b5c4f25e20d863a4aba572d9af6e7e9b
vinsmake <ps.enrique.e@gmail.com>
2023-08-02 15:27:02 -0600

    Project updated

--- src/TaylorSwiftErasTour.jsx
+++ src/TaylorSwiftErasTour.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import './normalize.css';
 import './TaylorSwiftErasTour.css';
 import { img1, img2, img3, img4, img5, img6, img7, img8, img9 } from './assets/img'
@@ -11,39 +10,39 @@ export const TaylorSwiftErasTour = () => {
     return (
         <>
 
-            <div>
+            <main className='main'>
                 <section className="section">
                     <div className="column1">
-                        <Image img={img1}/>
-                        <Image img={img2}/>
-                        <Image img={img3}/>
+                        <Image img={img1} />
+                        <Image img={img2} />
+                        <Image img={img3} />
                     </div>
                     <div className="column1">
-                    <Image img={img4}/>
+                        <Image img={img4} />
                         <article className="article">
                         </article>
-                        <Image img={img5}/>
+                        <Image img={img5} />
                     </div>
                     <div className="column1">
-                    <Image img={img6}/>
+                        <Image img={img6} />
                         <article className="article">
                         </article>
-                        <Image img={img7}/>
+                        <Image img={img7} />
 
                     </div>
                     <div className="column1">
-                    <Image img={img8}/>
+                        <Image img={img8} />
                         <article className="article">
                         </article>
-                        <Image img={img9}/>
-
+                        <Image img={img9} />
                     </div>
-
-
+                    <div className='texture'>
+                    </div>
                 </section>
+
                 {/*                 <input type="file" accept="image/*" onChange={handleImageUpload} />
                 <button onClick={downloadCanvas}>Download Image</button> */}
-            </div>
+            </main>
         </>
     )
 }
